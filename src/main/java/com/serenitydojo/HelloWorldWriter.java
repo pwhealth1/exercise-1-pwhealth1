2d1b9170508580ae9089629e55670c3b528a9740
pwhealth1 <pwhealth1@gmail.com>
2022-10-15 22:25:57 -0400

    Updated the test so that timeJump was the difference between the initial year and 2015

---
+++ src/main/java/com/serenitydojo/HelloWorldWriter.java
@@ -0,0 +1,10 @@
+package com.serenitydojo;
+
+public class HelloWorldWriter {
+    
+    public void helloWorldWriter(){
+        
+        final int age =43;
+        System.out.println("age is " + age);
+    }
+}
---
+++ src/test/java/com/serenitydojo/HelloWorldWriterTest.java
@@ -0,0 +1,25 @@
+package com.serenitydojo;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+public class HelloWorldWriterTest {
+    
+    @Test
+    public void validatingIntegerWasWriter() {
+        HelloWorldWriter writer = new HelloWorldWriter();
+        writer.helloWorldWriter();
+    }
+    @Test
+    public void whenWorkingWIthWholeNumbers(){
+        int initialYear = 1985;
+        int targetYear = 0;
+        int timeJump = 2015 - 1985;
+        targetYear +=initialYear + timeJump;
+        
+        
+        MatcherAssert.assertThat(targetYear, CoreMatchers.equalTo(2015));
+    }
+    
+}
